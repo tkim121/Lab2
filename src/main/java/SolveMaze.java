e0549250bd4c30e1291430105d2b2a026f3f8470
Tae Soo <wwne1232@gmail.com>
2018-01-30 16:28:33 -0600

    done!

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,25 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnLeft();
+                if (maze.canMove() == true) {
+                    maze.move();
+                } else {
+                    maze.turnRight();
+                    if (maze.canMove() == true) {
+                        maze.move();
+                    } else {
+                        maze.turnRight();
+                        if (maze.canMove() == true) {
+                            maze.move();
+                        } else {
+                            maze.turnRight();
+                            maze.move();
+                        }
+                    }
+                }
+            }
         }
 
         if (maze.isFinished()) {
